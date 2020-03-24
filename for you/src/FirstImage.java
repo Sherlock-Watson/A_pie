fa35b457aa8cd79e70bd1bf650e0c24a088548e3
Sherlock_Watson <19302010069@fudan.edu.cn>
2020-03-24 20:06:03 +0800

    web

--- for you/src/FirstImage.java
+++
@@ -1,5 +0,0 @@
-import javafx.stage.Stage;
-
-public class FirstImage {
-//    public Stage firstImage = new Stage();
-}
--- for you/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-//        FirstImage firstImage = new FirstImage();
-//        firstImage.firstImage.show();
-    }
-}
